663c4545fd59ab39b3a327a7f6cc1e22f8ac8e9b
Taras Rodynenko <t.rodynenko@gmail.com>
2022-10-23 20:56:47 +0200

    adjust linting

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	env: {
+		browser: true,
+	},
+	extends: [
+    'react-app',
+    'react-app/jest',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+    'semi': ['error', 'never'],
+		'react/no-unknown-property': ['error', { ignore: ['class'] }],
+		'react/prop-types': 0,
+		'react/display-name': 0,
+		'@typescript-eslint/explicit-function-return-type': 0,
+		'@typescript-eslint/explicit-module-boundary-types': 0,
+		'@typescript-eslint/no-explicit-any': ['error', { ignoreRestArgs: true }],
+		'@typescript-eslint/ban-ts-ignore': 0,
+		'@typescript-eslint/ban-ts-comment': 0,
+		'@typescript-eslint/no-explicit-any': 0,
+		'@typescript-eslint/no-empty-interface': 1,
+	}
+};
