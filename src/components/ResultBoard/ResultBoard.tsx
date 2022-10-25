f7e22bf74819fc1918013ea52316c36b1369db9c
Taras Rodynenko <t.rodynenko@gmail.com>
2022-10-25 21:35:42 +0200

    use table for ResultBoard

--- src/components/ResultBoard/ResultBoard.tsx
+++ src/components/ResultBoard/ResultBoard.tsx
@@ -2,28 +2,58 @@ import styled from '@emotion/styled'
 import React, { memo } from 'react'
 import { useSelector } from 'react-redux'
 import { selectUserItemsList } from 'redux/selectors/game'
-import ResultBoardRow from './components/ResultBoardRow'
 
-const ResultBoardList = styled.ol`
-  margin: 0;
-  padding: 0;
-  list-style: none;
+const ResultBoardTable = styled.table`
+  text-align: left;
+  position: relative;
+  border-collapse: collapse;
 `
 
-const ResultBoardItem = styled.li`
-  width: 100%;
+const ResultBoardRow = styled.tr`
+  thead & {
+    background-color: #eee;
+  }
+
+  & + & {
+    border-top: 1px dashed #999;
+  }
+`
+
+const ResultBoardHead = styled.th`
+  position: sticky;
+  top: 0;
+  text-align: center;
+  font-weight: 700;
+  padding: 1rem 0.5rem;
+`
+
+const ResultBoardCol = styled.td`
+  text-align: center;
+  padding: 0.5rem;
 `
 
 const ResultBoard = memo(() => {
   const items = useSelector(selectUserItemsList)
   return (
-    <ResultBoardList>
-      {items.map((item) => (
-        <ResultBoardItem key={item.key}>
-          <ResultBoardRow label={item.label} amount={item.amount} totalPoints={item.totalPoints} />
-        </ResultBoardItem>
-      ))}
-    </ResultBoardList>
+    <ResultBoardTable>
+      <caption className="sr-only">User points:</caption>
+      <thead>
+        <ResultBoardRow>
+          <ResultBoardHead scope="col">Item</ResultBoardHead>
+          <ResultBoardHead scope="col">Qty</ResultBoardHead>
+          <ResultBoardHead scope="col">Score</ResultBoardHead>
+        </ResultBoardRow>
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <ResultBoardRow key={item.key}>
+            <ResultBoardCol>{item.label}</ResultBoardCol>
+            <ResultBoardCol>{item.amount}</ResultBoardCol>
+            <ResultBoardCol>{item.totalPoints}</ResultBoardCol>
+          </ResultBoardRow>
+        ))}
+      </tbody>
+    </ResultBoardTable>
   )
 })
 
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -16,4 +16,15 @@ export const globalStyles = css`
     line-height: 1.3;
     color: var(--text-color);
   }
+
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0,0,0,0);
+    border: 0;
+  }
 `
