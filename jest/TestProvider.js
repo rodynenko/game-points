833f9b81394428e35434b06c990ebc8904803d3d
Taras Rodynenko <t.rodynenko@gmail.com>
2022-10-25 21:07:10 +0200

    add items to test store

--- jest/TestProvider.js
+++ jest/TestProvider.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import store from 'redux/store'
 import { Provider } from 'react-redux'
+import { addNewUserItem } from 'redux/reducers/gameReducer'
+
+store.dispatch(addNewUserItem('a'))
+store.dispatch(addNewUserItem('b'))
+store.dispatch(addNewUserItem('a'))
 
 export const TestProvider = ({ children }) => (
   <Provider store={store}>
